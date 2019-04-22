9ee7e70d26da62bfd9b373ddca89cfb338a8b826
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-04-22 15:20:40 -0400

    Returning object from controller

---
+++ src/main/java/io/example/springbootstarter/topic/TopicController.java
@@ -0,0 +1,24 @@
+package io.example.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics()
+	{
+		List<Topic> list = new ArrayList<>();
+		Topic t1 = new Topic("1","java","Description of java");
+		Topic t2 = new Topic("2","Spring","Description of Spring");
+		Topic t3 = new Topic("3","Algorithms","Description of algorithm");
+		list.add(t1);
+		list.add(t2);
+		list.add(t3);
+		return list;
+	}
+}
