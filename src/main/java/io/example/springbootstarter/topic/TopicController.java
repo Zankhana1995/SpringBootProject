302747b66a481e4e98d838dd9b0abfa42062d28e
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-04-22 17:13:28 -0400

    Getting single resource

--- src/main/java/io/example/springbootstarter/topic/TopicController.java
+++ src/main/java/io/example/springbootstarter/topic/TopicController.java
@@ -1,9 +1,10 @@
 package io.example.springbootstarter.topic;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class TopicController {
 //		list.add(t3);
 
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id)
+	{
+		return topicService.getTopic(id);
 	}
 }
