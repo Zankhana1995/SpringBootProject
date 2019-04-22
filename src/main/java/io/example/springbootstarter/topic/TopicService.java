629eb0479465b27fedc0a873e65d00d2d06ce091
Zankhana Patel <41522189+Zankhana1995@users.noreply.github.com>
2019-04-22 19:12:59 -0400

    HTTP post example

--- src/main/java/io/example/springbootstarter/topic/TopicService.java
+++ src/main/java/io/example/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.example.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,16 +8,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(new Topic("1", "java", "Description of java"),
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("1", "java", "Description of java"),
 			new Topic("2", "Spring", "Description of Spring"),
-			new Topic("3", "Algorithms", "Description of algorithm"));
+			new Topic("3", "Algorithms", "Description of algorithm")));
 
 	public List<Topic> getAllTopics() {
 		return topics;
 	}
-	
-	public Topic getTopic(String id)
-	{
+
+	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+
+		topics.add(topic);
+		System.out.println(topics);
 	}
 }
